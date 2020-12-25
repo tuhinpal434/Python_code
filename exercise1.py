35d8bb0f2edbbce74d27bcf4945bd64fc5660d57
Tuhin Pal <tuhinpal.434@gmail.com>
2020-12-25 16:25:58 +0530

    completed exercise one

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+#this is \\ double backslash
+print("this is \\\ double backslash")
+# these are /\/\/\/\/\ mountains
+print("these are /\\/\\/\\/\\/\\ mountains")
+# he is     awesome
+print("he is \tawesome")
+# \" \n \t \'
+print("\\\" \\n \\t \\\'")
